850b78360195ea0bee875a6abdb9d140105bc84d
Peradon <peradon_charoenchainetr@hotmail.com>
2016-04-18 20:09:25 +0700

    ThirdPush

--- Project/settings.py
+++ Project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Somchai.apps.AppConfig',
+    'SomchaiApp.apps.SomchaiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
