f8803b74e2ca47f014ffe7ea37d33a6e6d5e2e4b
Jr <jira.jiravaraphan@gmail.com>
2016-04-27 18:41:09 +0700

    Open login function

--- SomchaiApp/models.py
+++ SomchaiApp/models.py
@@ -9,3 +9,17 @@ class UserModel(models.Model):
     user = models.OneToOneField(User, unique=True, primary_key=True)
     department = models.CharField(max_length=50, default="None")
     position = models.CharField(max_length=50, default="None")
+
+class TodoList(models.Model):
+    #employee
+    user=models.ForeignKey(User,unique=True,primary_key=True)
+    #task description
+    taskDescription = models.CharField(max_length=100)
+    start=models.DateTimeField()
+    end=models.DateTimeField()
+
+class Reservation(models.Model):
+    topic=models.CharField(max_length=50)
+    start=models.DateTimeField()
+    end=models.DateTimeField()
+    room=models.CharField(max_length=50)
--- SomchaiApp/urls.py
+++ SomchaiApp/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^login', views.authen, name='authen'),
     url(r'^register', views.register, name='register'),
-    url(r'^logout', views.logout, name='logout')
+    url(r'^logout', views.logout, name='logout'),
+    #****
+    url(r'^todo',views.todo,name='todo'),
 ]
